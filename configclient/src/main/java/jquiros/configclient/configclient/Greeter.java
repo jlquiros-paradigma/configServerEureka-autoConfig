ddec9757f0afe0bd6ce609be944db7380cc10ab0
JL Quiros <jlquiros@paradigmadigital.com>
2017-07-27 08:46:16 +0200

    cleaning

--- configclient/src/main/java/jquiros/configclient/configclient/Greeter.java
+++ configclient/src/main/java/jquiros/configclient/configclient/Greeter.java
@@ -1,6 +1,9 @@
 package jquiros.configclient.configclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,4 +55,14 @@ public class Greeter {
     {
         return this.pass.equals(pass)?"OK":"KO";
     }
+
+
+    /*@Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }*/
 }
